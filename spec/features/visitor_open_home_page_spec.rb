342b77275d84c3bdd50b0f0328f43cd8ccc63813
Ale <lima.ale@gmail.com>
2019-12-16 19:24:01 -0300

    Started project and configured rspec and capybara

---
+++ spec/features/visitor_open_home_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'Visitor open home page' do
+  scenario 'successfully' do
+    visit root_path
+
+    expect(page).to have_content('My New Job')
+    expect(page).to have_content('Bem vindo ao My New Job, aqui você encontra seu novo emprego')
+  end
+end
